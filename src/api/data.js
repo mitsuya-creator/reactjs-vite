2ae9786408952b161f060c66cfe3f524966c81f1
@code <jj4068851@gmail.com>
2023-07-18 10:48:03 +0700

    dataSample

---
+++ src/api/data.js
@@ -0,0 +1,14 @@
+const dataSample = [
+    {
+        name: "Mitsuya Takashi",
+        imgUrl: "https://github.com/mitsuya-creator/reactJS/blob/main/src/assets/Mitsuya.jpeg"
+    },
+    {
+        name: "Tachibana Hina",
+        imgUrl: "https://github.com/mitsuya-creator/reactJS/blob/main/src/assets/hina.jpg"
+    },
+    {
+        name: "Chifuyu Matsuno",
+        imgUrl: "https://github.com/mitsuya-creator/reactJS/blob/main/src/assets/cifuyu.jpg"
+    }
+]
