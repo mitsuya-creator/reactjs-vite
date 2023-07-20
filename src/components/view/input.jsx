94b9bf6d289ba0c90d9bc86cc4655d9c646da715
@code <jj4068851@gmail.com>
2023-07-20 08:15:09 +0700

    useImperativeHandle

--- src/components/view/input.jsx
+++ src/components/view/input.jsx
@@ -1,14 +1,19 @@
 import FocusBtn from "../button/focus";
-import { useRef, forwardRef } from "react";
+import { useRef, forwardRef, useImperativeHandle } from "react";
 
 const MyInput = forwardRef((props, ref) => {
-    return <input type="text" {...props} ref={ref} />
+    const myRef = useRef(null);
+    useImperativeHandle(ref, () => ({
+        focus() {
+            myRef.current.focus();
+        }
+    }))
+    return <input type="text" {...props} ref={myRef} />
 })
 export default function InputField() {
     const myref = useRef(null);
     const hanldeClick = () => {
         myref.current.focus();
-        console.log(myref.current.attributes);
     }
     return (
         <>
