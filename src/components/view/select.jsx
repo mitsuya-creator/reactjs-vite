0840c204a7258bde6fbedddc0127b3f93a3a4dd4
@code <jj4068851@gmail.com>
2023-07-23 09:55:13 +0700

    feat(select): refactoring to select component

--- src/components/view/select.jsx
+++ src/components/view/select.jsx
@@ -1,4 +1,15 @@
-export default function SelectPerson({ setPerson, person, bio }) {
+import React, { useState, useEffect } from "react";
+import { fetchBio } from "../../utils/apiDummy";
+
+export default function SelectPerson() {
+    const [person, setPerson] = useState("Mitsuya");
+    const [bio, setBio] = useState(null);
+    useEffect(() => {
+        setBio(null);
+        let ignore = false;
+        fetchBio(person).then(response => !ignore && setBio(response));
+        return () => ignore = true;
+    }, [person])
     return (
         <>
             <select value={person} onChange={(e) => setPerson(e.target.value)}>
--- src/features/App.jsx
+++ src/features/App.jsx
@@ -1,20 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { fetchBio } from "../utils/apiDummy";
 import SelectPerson from "../components/view/select";
 
 export default function App() {
-  const [person, setPerson] = useState("Mitsuya");
-  const [bio, setBio] = useState(null);
-  console.log(person)
-  useEffect(() => {
-    setBio(null);
-    let ignore = false;
-    fetchBio(person).then(response => !ignore && setBio(response));
-    return () => ignore = true;
-  }, [person])
+
   return (
     <>
-      <SelectPerson setPerson={setPerson} person={person} bio={bio} />
+      <SelectPerson />
     </>
   )
 }
