2340f485622815df23bf3bba279953d1d29fbeca
@code <jj4068851@gmail.com>
2023-07-28 07:56:30 +0700

    feat(all): populate a chain of select box effect

--- src/features/App.jsx
+++ src/features/App.jsx
@@ -1,27 +1,55 @@
-import React, { useState } from "react";
-import ChatRoom from "../components/view/chatRooms";
+import React, { useState, useEffect } from 'react';
+import { fetchData } from '../utils/apiPlanet';
 
 export default function App() {
-  const [roomId, setRoomId] = useState('general');
-  const [encryption, setEncryption] = useState(false);
+  const [planetList, setPlanetList] = useState([]);
+  const [planetId, setPlanetId] = useState('');
+
+  const [placeList, setPlaceList] = useState([]);
+  const [placeId, setPlaceId] = useState('');
+
+  useEffect(() => {
+    let ignore = false;
+    fetchData('/planets').then(response => {
+      if (!ignore) {
+        console.log('fetched a list of planets');
+        setPlanetList(response);
+        setPlanetId(response[0].id)
+      }
+    })
+    return () => ignore = true;
+  }, []);
+
+  useEffect(() => {
+    if (planetId === '') return;
+    let ignore = false;
+    fetchData(`/planets/${planetId}/places`).then(response => {
+      if (!ignore) {
+        console.log(`fetched a list places of ${planetId}`);
+        setPlaceList(response);
+        setPlaceId(response[0].id)
+      }
+    })
+    return () => ignore = true;
+  }, [planetId]);
 
   return (
     <>
       <label>
-        choose the chat room {''}
-        <select value={roomId} onChange={e => setRoomId(e.target.value)}>
-          <option value="general">general</option>
-          <option value="game">game</option>
-          <option value="travel">travel</option>
+        pick a planet:
+        <select value={planetId} onChange={e => setPlanetId(e.target.value)}>
+          {planetList.map(planet => <option key={planet.id} value={planet.id}>{planet.name}</option>)}
         </select>
       </label>
       <br />
       <label>
-        <input type="checkbox" checked={encryption} onChange={e => setEncryption(e.target.checked)} />
-        Enable encryption
+        pick a place:
+        <select value={placeId} onChange={e => setPlaceId(e.target.value)}>
+          {placeList.map(place => <option value={place.id} key={place.id}>{place.name}</option>)}
+        </select>
       </label>
       <hr />
-      <ChatRoom encryp={encryption} roomId={roomId} />
+      <h1>you are going to: {placeId.charAt(0).toUpperCase() + placeId.slice(1) || '???'} on {planetId.charAt(0).toUpperCase() + planetId.slice(1) || '???'}</h1>
     </>
   )
 }
