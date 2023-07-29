d595cd388759dbd566c4f8b6125ba1ff2ae82d2e
@code <jj4068851@gmail.com>
2023-07-29 10:13:25 +0700

    feat(all): fix variable that does not update (separating event from effect)

--- src/features/App.jsx
+++ src/features/App.jsx
@@ -1,55 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { fetchData } from '../utils/apiPlanet';
+import React, { useState, useEffect } from "react";
 
 export default function App() {
-  const [planetList, setPlanetList] = useState([]);
-  const [planetId, setPlanetId] = useState('');
-
-  const [placeList, setPlaceList] = useState([]);
-  const [placeId, setPlaceId] = useState('');
-
+  const [counter, setCounter] = useState(0);
+  const [increment, setIncrement] = useState(1);
   useEffect(() => {
-    let ignore = false;
-    fetchData('/planets').then(response => {
-      if (!ignore) {
-        console.log('fetched a list of planets');
-        setPlanetList(response);
-        setPlanetId(response[0].id)
-      }
-    })
-    return () => ignore = true;
-  }, []);
-
-  useEffect(() => {
-    if (planetId === '') return;
-    let ignore = false;
-    fetchData(`/planets/${planetId}/places`).then(response => {
-      if (!ignore) {
-        console.log(`fetched a list places of ${planetId}`);
-        setPlaceList(response);
-        setPlaceId(response[0].id)
-      }
-    })
-    return () => ignore = true;
-  }, [planetId]);
-
+    let counterId = setInterval(() => {
+      setCounter(c => c + increment)
+    }, 1000)
+    return () => clearInterval(counterId);
+  }, [increment]);
   return (
     <>
-      <label>
-        pick a planet:
-        <select value={planetId} onChange={e => setPlanetId(e.target.value)}>
-          {planetList.map(planet => <option key={planet.id} value={planet.id}>{planet.name}</option>)}
-        </select>
-      </label>
-      <br />
-      <label>
-        pick a place:
-        <select value={placeId} onChange={e => setPlaceId(e.target.value)}>
-          {placeList.map(place => <option value={place.id} key={place.id}>{place.name}</option>)}
-        </select>
-      </label>
+      <h1>Counter: {counter} </h1>
+      <button type="buton">Reset</button>
       <hr />
-      <h1>you are going to: {placeId.charAt(0).toUpperCase() + placeId.slice(1) || '???'} on {planetId.charAt(0).toUpperCase() + planetId.slice(1) || '???'}</h1>
+      <span>Every second, increment by:</span>
+      <button type="button" onClick={() => setIncrement(i => i - 1)} disabled={increment === 0}>-</button>
+      {increment}
+      <button type="button" onClick={() => setIncrement(i => i + 1)}>+</button>
     </>
   )
 }
