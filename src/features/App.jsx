748b77ce477548c504fb903f75e0e577695917e6
@code <jj4068851@gmail.com>
2023-07-30 07:51:52 +0700

    feat(delayCounter): fix a non-adjustable delay useEffectEvent

--- src/features/App.jsx
+++ src/features/App.jsx
@@ -4,13 +4,14 @@ import { experimental_useEffectEvent as useEffectEvent } from "react"; //use rea
 export default function App() {
   const [counter, setCounter] = useState(0);
   const [increment, setIncrement] = useState(1);
+  const [delay, setDelay] = useState(100);
   const onTick = useEffectEvent(() => setCounter(c => c + increment));
   useEffect(() => {
     let counterId = setInterval(() => {
       onTick();
-    }, 1000)
+    }, delay)
     return () => clearInterval(counterId);
-  }, []);
+  }, [delay]);
   return (
     <>
       <h1>Counter: {counter} </h1>
@@ -18,8 +19,14 @@ export default function App() {
       <hr />
       <span>Every second, increment by:</span>
       <button type="button" onClick={() => setIncrement(i => i - 1)} disabled={increment === 0}>-</button>
-      {increment}
+      <b style={{ fontSize: 24, margin: 20 }}>{increment}</b>
       <button type="button" onClick={() => setIncrement(i => i + 1)}>+</button>
+      <br />
+      <span>Increment delay: <button type="button" style={{ fontSize: 16 }} onClick={() => setDelay(d => d - 100)}>-{100}</button>
+      </span>
+      <b style={{ fontSize: 16, marginLeft: 10 }}>{delay} ms</b>
+      <br />
+      <button type="button" style={{ fontSize: 16 }} onClick={() => setDelay(d => d + 100)}>+{100}</button>
     </>
   )
 }
