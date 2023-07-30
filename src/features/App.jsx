79f4382ab2f0f6ada757c649a274416480c69c4a
@code <jj4068851@gmail.com>
2023-07-30 07:26:45 +0700

    feat(hooks): fix a freezing counter using useEffectEvent hook

--- src/features/App.jsx
+++ src/features/App.jsx
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from "react";
+import { experimental_useEffectEvent as useEffectEvent } from "react"; //use react@experimental verison
 
 export default function App() {
   const [counter, setCounter] = useState(0);
   const [increment, setIncrement] = useState(1);
+  const onTick = useEffectEvent(() => setCounter(c => c + increment));
   useEffect(() => {
     let counterId = setInterval(() => {
-      setCounter(c => c + increment)
+      onTick();
     }, 1000)
     return () => clearInterval(counterId);
-  }, [increment]);
+  }, []);
   return (
     <>
       <h1>Counter: {counter} </h1>
-      <button type="buton">Reset</button>
+      <button type="buton" onClick={() => setCounter(0)}>Reset</button>
       <hr />
       <span>Every second, increment by:</span>
       <button type="button" onClick={() => setIncrement(i => i - 1)} disabled={increment === 0}>-</button>
