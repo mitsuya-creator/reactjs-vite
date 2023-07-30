9d175f1bf2288b0fd728f77cda2dd06a6e934d7b
@code <jj4068851@gmail.com>
2023-07-30 09:39:11 +0700

    feat(content): set show toastify container

--- src/features/App.jsx
+++ src/features/App.jsx
@@ -1,32 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { experimental_useEffectEvent as useEffectEvent } from "react"; //use react@experimental verison
+import React, { useState } from "react";;
+import { toast } from "react-toastify";
+import ShowNotification from "../components/view/notification";
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-  const [increment, setIncrement] = useState(1);
-  const [delay, setDelay] = useState(100);
-  const onTick = useEffectEvent(() => setCounter(c => c + increment));
-  useEffect(() => {
-    let counterId = setInterval(() => {
-      onTick();
-    }, delay)
-    return () => clearInterval(counterId);
-  }, [delay]);
+  const [show, setShow] = useState(false);
   return (
     <>
-      <h1>Counter: {counter} </h1>
-      <button type="buton" onClick={() => setCounter(0)}>Reset</button>
-      <hr />
-      <span>Every second, increment by:</span>
-      <button type="button" onClick={() => setIncrement(i => i - 1)} disabled={increment === 0}>-</button>
-      <b style={{ fontSize: 24, margin: 20 }}>{increment}</b>
-      <button type="button" onClick={() => setIncrement(i => i + 1)}>+</button>
-      <br />
-      <span>Increment delay: <button type="button" style={{ fontSize: 16 }} onClick={() => setDelay(d => d - 100)}>-{100}</button>
-      </span>
-      <b style={{ fontSize: 16, marginLeft: 10 }}>{delay} ms</b>
-      <br />
-      <button type="button" style={{ fontSize: 16 }} onClick={() => setDelay(d => d + 100)}>+{100}</button>
+      <button type="button" onClick={() => {
+        toast.success("easy");
+        setShow(!show);
+      }}>Notify</button>
+      {show && <ShowNotification />}
+
     </>
   )
 }
