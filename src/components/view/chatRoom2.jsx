43464cd424e7edf19e9588add91efc6559babaa8
@code <jj4068851@gmail.com>
2023-08-01 10:12:17 +0700

    fix(showNotification): fixed show notification using toastify-js

--- src/components/view/chatRoom2.jsx
+++ src/components/view/chatRoom2.jsx
@@ -1,42 +1,27 @@
 import React, { experimental_useEffectEvent as useEffectEvent } from "react";
-import ShowNotification from "./notification";
-import { useEffect, useState } from "react";
+import { showNotification } from "./notification"
+import { useEffect } from "react";
 import { creatConnection } from "../../utils/chat2";
 
 export default function ChatRoom({ roomId, isDark }) {
-    const [show, setShow] = useState(false)
-    const [message, setMessage] = useState("");
-    const onConnected = useEffectEvent(() => setShow(true))
+    const onConnected = useEffectEvent((e) => showNotification(e, isDark));
     useEffect(() => {
-        let ignore = false;
-        let timeout;
-        setMessage(null);
+
         const connection = creatConnection('http://localhost:', roomId);
         try {
-            connection.on("connected", () => {
-                timeout = setTimeout(() => {
-                    if (!ignore) setMessage(`Welcome to ${roomId}`);
-                    onConnected()
+            connection.on("connecte", () => {
+                setTimeout(() => {
+                    onConnected(`Welcome to ${roomId}`);
                 }, 1000)
             });
             connection.connect();
         } catch (e) {
-            console.log(e.message);
-            setMessage(e.message);
-            setTimeout(() => onConnected(), 1000);
+            setTimeout(() => onConnected(e.message), 1000);
         }
         return () => {
-            ignore = true;
-            clearTimeout(timeout);
             connection.disconnect();
         }
 
     }, [roomId]);
-
-    return (
-        <>
-            <h1>Welcome to {roomId} room !</h1>
-            {show && <ShowNotification msg={message} isDark={isDark} roomId={roomId} />}
-        </>
-    )
+    return <h1>Welcome to {roomId} room !</h1>
 }
