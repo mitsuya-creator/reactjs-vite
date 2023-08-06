e1304fbc06b644700903e2c94abba531908d4d30
@code <jj4068851@gmail.com>
2023-08-07 06:42:51 +0700

    feat: custom hooks useOnlineStatus

--- src/features/App.jsx
+++ src/features/App.jsx
@@ -1,38 +1,23 @@
-import React, { useState } from "react";
-import { showNotification } from "../utils/notification";
-import ChatRoom from "../components/view/chatRoom3";
+import React from "react";
+import { useOnlineStatus } from "../hooks/useOnline";
+
+function StatusBar() {
+  const isOnline = useOnlineStatus();
+  return <h1>{isOnline ? " Online" : "Offline"}</h1>
+}
+
+function SaveButton() {
+  const isOnline = useOnlineStatus();
+
+  return <button type="button" disabled={!isOnline} onClick={() => console.log("save progress")}>{isOnline ? "Save Progress" : "Reconnecting"}</button>
+}
+
 
 export default function App() {
-  const [isDark, setIsDark] = useState(false);
-  const [isEncrypted, setIsEncrypted] = useState(false);
-  const [roomId, setRoomId] = useState("general");
-
   return (
     <>
-      <label>
-        <input type="checkbox" checked={isDark} onChange={e => setIsDark(e.target.checked)} />
-        Use dark theme
-      </label>
-      <br />
-      <label>
-        <input type="checkbox" checked={isEncrypted} onChange={e => setIsEncrypted(e.target.checked)} />
-        Enable encryption
-      </label>
-      <br />
-      <label>
-        Choose the chat room :
-        <select value={roomId} onChange={e => setRoomId(e.target.value)}>
-          <option value="general">general</option>
-          <option value="music">music</option>
-          <option value="travel">travel</option>
-        </select>
-      </label>
-      <hr />
-      <ChatRoom
-        roomId={roomId}
-        isEncrypted={isEncrypted}
-        onMessage={msg => showNotification(`New Message: ${msg}`, isDark)}
-      />
+      <SaveButton />
+      <StatusBar />
     </>
   )
 }
