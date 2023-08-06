3a42a5b7ab96f6d1460ccd5b15d6e33a86704c42
@code <jj4068851@gmail.com>
2023-08-06 08:35:45 +0700

    feat(hook): add useEffect and useEffectEvent

--- src/components/view/chatRoom3.jsx
+++ src/components/view/chatRoom3.jsx
@@ -1,1 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, experimental_useEffectEvent as useEffectEvent } from "react";
+import { createEncryption, createUnEncryption } from "../../utils/chat4";
+
+export default function ChatRoom({ roomId, isEncrypted, onMessage }) {
+    const onReceiveMessage = useEffectEvent(onMessage);
+
+    useEffect(() => {
+        const createConnection = () => {
+            const options = {
+                serverUrl: 'localhost:678',
+                roomId: roomId
+            }
+            if (isEncrypted) {
+                return createEncryption(options)
+            } else {
+                return createUnEncryption(options)
+            }
+        }
+
+        const connection = createConnection();
+        connection.on("message", (msg) => onReceiveMessage(msg));
+        connection.connect()
+        return () => connection.disconnect();
+
+    }, [roomId, isEncrypted])
+
+    return <h1>Welcome to {roomId}</h1>
+}
