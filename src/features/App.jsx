ee11be152a96d9ef8f581a7431290ede388b3a04
@code <jj4068851@gmail.com>
2023-08-07 10:00:01 +0700

    feat(state): add state delay configurable

--- src/features/App.jsx
+++ src/features/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { useCounter } from "../hooks/useCounter";
 
 export default function App() {
-  const counter = useCounter(1);
-  return <h1>Second passed : {counter}</h1>
+  const [delay, setDelay] = useState(1000)
+  const counter = useCounter(1, delay);
+
+  return (
+    <>
+      <label>
+        Tick duration: {delay} ms:
+        <input
+          type="range"
+          min="1000"
+          max="3000"
+          value={delay}
+          onChange={e => setDelay(e.target.value)}
+        />
+      </label>
+      <hr />
+      <h1>Tick : {counter}</h1>
+    </>
+  )
 }
