0e72237d2f1cbafb94eefad7237c2f0ef507a4be
@code <jj4068851@gmail.com>
2023-08-07 08:14:07 +0700

    feat(hooks): replace useEffect to custom hooks useChatRoom (#5)

--- src/components/view/chatRoom3.jsx
+++ src/components/view/chatRoom3.jsx
@@ -1,28 +1,7 @@
-import React, { useEffect, experimental_useEffectEvent as useEffectEvent } from "react";
-import { createEncryption, createUnEncryption } from "../../utils/chat4";
+import React from "react";
+import { useChatRoom } from "../../hooks/useChatRoom";
 
 export default function ChatRoom({ roomId, isEncrypted, onMessage }) {
-    const onReceiveMessage = useEffectEvent(onMessage);
-
-    useEffect(() => {
-        const createConnection = () => {
-            const options = {
-                serverUrl: 'localhost:678',
-                roomId: roomId
-            }
-            if (isEncrypted) {
-                return createEncryption(options)
-            } else {
-                return createUnEncryption(options)
-            }
-        }
-
-        const connection = createConnection();
-        connection.on("message", (msg) => onReceiveMessage(msg));
-        connection.connect()
-        return () => connection.disconnect();
-
-    }, [roomId, isEncrypted])
-
+    useChatRoom(roomId, isEncrypted, onMessage);
     return <h1>Welcome to {roomId}</h1>
 }
