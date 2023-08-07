3087f11b35a6bbae171802fae0122ebdd54888bf
@code <jj4068851@gmail.com>
2023-08-07 09:51:16 +0700

    feat(content): useCounter custom hooks

--- src/features/App.jsx
+++ src/features/App.jsx
@@ -1,36 +1,7 @@
-import React, { useState } from "react";
-import ChatRoom from "../components/view/chatRoom3";
-import { showNotification } from "../utils/notification";
+import React from "react";
+import { useCounter } from "../hooks/useCounter";
 
 export default function App() {
-  const [roomId, setRoomId] = useState("general");
-  const [isEncrypted, setIsEncrypted] = useState(false);
-  const [isDark, setIsDark] = useState(false);
-
-  return (
-    <>
-      <label>
-        Choose the chat room : {""}
-        <select value={roomId} onChange={e => setRoomId(e.target.value)}>
-          <option value="general">general</option>
-          <option value="travel">travel</option>
-          <option value="music">music</option>
-        </select>
-      </label>
-      <hr />
-      <label>
-        <input type="checkbox" checked={isEncrypted} onChange={e => setIsEncrypted(e.target.checked)} /> Enable Encryption
-      </label>
-      <br />
-      <label>
-        <input type="checkbox" checked={isDark} onChange={e => setIsDark(e.target.checked)} /> Enable dark mode
-      </label>
-      <br />
-      <ChatRoom
-        roomId={roomId}
-        isEncrypted={isEncrypted}
-        onMessage={msg => showNotification(`New message: ${msg}`, isDark)}
-      />
-    </>
-  )
+  const counter = useCounter(1);
+  return <h1>Second passed : {counter}</h1>
 }
