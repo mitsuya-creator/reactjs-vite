e1823f4b9a86b0dea18a5ca23efc4fbfc287a63c
@code <jj4068851@gmail.com>
2023-08-08 07:41:20 +0700

    feat(randomColor): useInterval for change random background color

--- src/features/App.jsx
+++ src/features/App.jsx
@@ -1,23 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import { useCounter } from "../hooks/useCounter";
+import { useInterval } from "../hooks/useInterval";
 
 export default function App() {
-  const [delay, setDelay] = useState(1000)
-  const counter = useCounter(1, delay);
+  const counter = useCounter(1, 1000);
+
+  useInterval(() => {
+    const randomColor = `hsla(${Math.random() * 360}, 100%, 50%, 1)`;
+    document.body.style.backgroundColor = randomColor;
+  }, 2000)
 
   return (
     <>
-      <label>
-        Tick duration: {delay} ms:
-        <input
-          type="range"
-          min="1000"
-          max="3000"
-          value={delay}
-          onChange={e => setDelay(e.target.value)}
-        />
-      </label>
-      <hr />
       <h1>Tick : {counter}</h1>
     </>
   )
