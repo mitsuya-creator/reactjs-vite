6de1b6f29b53cd436c9fa9b921a5a5f8b9391a79
@code <jj4068851@gmail.com>
2023-08-08 08:24:32 +0700

    feat: custom hooks usePointerPosition

---
+++ src/hooks/usePointerPosition.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+export function usePointerPosition() {
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    useEffect(() => {
+        const handleMove = e => setPosition({ x: e.clientX, y: e.clientY });
+        window.addEventListener("pointermove", handleMove);
+        return () => window.removeEventListener("pointermove", handleMove);
+    }, []);
+    return position;
+}
